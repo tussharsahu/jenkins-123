6e7de6c99253c31930a44aabb6764356b9de0b81
root <root@Thakur>
2024-04-12 18:02:11 +0530

    Your commit message

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 source env/bin/activate
+
+cd /var/lib/jenkins/workspace/django/app
+
+python3 manage.py makemigrations
+python3 manage.py migrate 
+
+echo "migrations done"
+
+cd /var/lib/jenkins/workspace/django
+
+source cp -rf gunicorn.socket /etc/systemd/system
+source cp -rf gunicorn.service /etc/systemd/system
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload 
+sudo systemctl start gunicorn 
+sudo systemctl enable gunicorn
+
+echo "gunicorn setup done"
+sudo systemctl status gnicorn
+sudo systemctl restart gnicorn
