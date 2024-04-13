235799629d08bbca7309e0773a1bdd8f14d8d17c
root <root@Thakur>
2024-04-13 19:32:19 +0530

    gunicorn.sh update

--- gunicorn.sh
+++ gunicorn.sh
@@ -11,8 +11,8 @@ echo "migrations done"
 
 cd /var/lib/jenkins/workspace/django
 
-source cp -rf gunicorn.socket /etc/systemd/system
-source cp -rf gunicorn.service /etc/systemd/system
+cp -rf gunicorn.socket /etc/systemd/system
+cp -rf gunicorn.service /etc/systemd/system
 
 echo "$USER"
 echo "$PWD"
@@ -21,10 +21,10 @@ SUDO_ASKPASS=./myaskpass.sh
 export SUDO_ASKPASS
 
 
- echo "Aryan123#" | sudo -S systemctl daemon-reload
- sudo systemctl start gunicorn 
- sudo systemctl enable gunicorn
+ echo "Aryan123#" | sudo -A systemctl daemon-reload
+ sudo -A systemctl start gunicorn 
+ sudo -A systemctl enable gunicorn
 
  echo "gunicorn setup done"
- sudo systemctl status gnicorn
- sudo systemctl restart gnicorn
+ sudo -A systemctl status gnicorn
+ sudo -A systemctl restart gnicorn
