c628dbe96572497ec6dfcdb68956526d283049fe
Aryan Thakur <149156092+AryanThakur41@users.noreply.github.com>
2024-04-15 14:22:48 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,7 +6,7 @@ chmod 710 /var/lib/jenkins/workspace/django
 sudo nginx -t 
 
 #sudo apt update -y
-sudo apt install nginx -y
+#sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
